b4b80c41fa7fd2895d899475718c2c831d61ef42
colby <colby88cardell@gmail.com>
2021-02-23 19:02:45 -0500

    :wrench: added TW forms

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+const api = {
+  async index(
+    endpoint = "http://my-json-server.typicode.com/claim-academy-js/products/products"
+  ) {
+    const res = await fetch;
+  },
+};
+
+export default api;
