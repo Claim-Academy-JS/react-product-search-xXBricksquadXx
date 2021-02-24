1ad733add75ce91f8bae44388c6278172583f364
colby <colby88cardell@gmail.com>
2021-02-23 19:03:13 -0500

    :seedling: Starter Code

---
+++ src/components/FilterableProductTable/index.js
@@ -0,0 +1,11 @@
+import Table from "./ProductTable";
+import Search from "./SearchBar";
+
+const FilterableProductTables = () => (
+  <main>
+    <Search />
+    <Table />
+  </main>
+);
+
+export default FilterableProductTables;
