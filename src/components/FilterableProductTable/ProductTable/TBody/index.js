ef29855e8b6125a97f9032e9e5c0daa1c8dc3f47
colby <colby88cardell@gmail.com>
2021-02-24 22:35:12 -0500

    :bug: Dependency array for `useEffect`

--- src/components/FilterableProductTable/ProductTable/TBody/index.js
+++ src/components/FilterableProductTable/ProductTable/TBody/index.js
@@ -11,13 +11,25 @@ function renderRows(products) {
 const TBody = () => {
   const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    (async () => {
-      // Await results of reading stream as JSON
-      const productData = await api.index();
-      setProducts(() => productData);
-    })();
-  });
+  useEffect(
+    () => {
+      (async () => {
+        // Await results of reading stream as JSON
+        const productData = await api.index();
+        setProducts(() => productData);
+      })();
+    },
+
+    /**
+     * 2nd argument to `useEffect` is an Array.
+     * This array tells `useEffect` what pieces of state it
+     * should watch.
+     *
+     * An empty array means that `useEffect` should never run again
+     * after initial load - as it's not watching any state.
+     */
+    []
+  );
 
   return <tbody>{renderRows(products)}</tbody>;
 };
