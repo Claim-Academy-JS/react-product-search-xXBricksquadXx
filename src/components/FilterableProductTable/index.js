6070bf46ac5c9051f88f42251464c8975185644c
colby <colby88cardell@gmail.com>
2021-02-24 22:39:00 -0500

    Apply Tailwind CSS for table

--- src/components/FilterableProductTable/index.js
+++ src/components/FilterableProductTable/index.js
@@ -1,11 +1,11 @@
 import Table from "./ProductTable";
 import Search from "./SearchBar";
 
-const FilterableProductTables = () => (
-  <main>
+const FilterableProductTable = () => (
+  <main className="container mx-auto w-1/2 flex flex-col items-center mt-8">
     <Search />
     <Table />
   </main>
 );
 
-export default FilterableProductTables;
+export default FilterableProductTable;
