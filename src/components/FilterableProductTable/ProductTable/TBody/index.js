0b69c736089d407cd18dc195b1bc8f87b41b00e8
colby <colby88cardell@gmail.com>
2021-02-24 22:49:05 -0500

    :sparkles: conditionally render red for out of stock

--- src/components/FilterableProductTable/ProductTable/TBody/index.js
+++ src/components/FilterableProductTable/ProductTable/TBody/index.js
@@ -1,48 +1,35 @@
-import api from "api";
-import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import Row from "./Row";
 
+// TBody will receive filtered products - and will send one at a time to Row for rendering
+
 function renderRows(products) {
-  return products.map(({ name, price, category }, index, currArr) =>
+  return products.map(({ name, price, category, stocked }, index, currArr) =>
     // Is the category of the previous item in the array the same as the current item's category?
     currArr[index - 1]?.category === currArr[index].category ? (
       // If category is same, JUST render the product.
-      <Row name={name} price={price} key={index} />
+      <Row name={name} price={price} key={index} stocked={stocked} />
     ) : (
       // If it's a new category, render 2 rows...
       <>
         <Row category={category} key={index} />
-        <Row name={name} price={price} key={index} />
+        <Row name={name} price={price} stocked={stocked} key={index} />
       </>
     )
   );
 }
 
-const TBody = () => {
-  const [products, setProducts] = useState([]);
+const TBody = ({ products }) => <tbody>{renderRows(products)}</tbody>;
 
-  useEffect(
-    () => {
-      (async () => {
-        // Await results of reading stream as JSON
-        const productData = await api.index();
-        setProducts(() => productData);
-      })();
-    },
-
-    /**
-     * 2nd argument to `useEffect` is an Array.
-     * This array tells `useEffect` what pieces of state it
-     * should watch.
-     *
-     * An empty array means that `useEffect` should never run again
-     * after initial load - as it's not watching any state.
-     */
-    []
-  );
-
-  // Trim duplicate categories from the catalog
-  return <tbody>{renderRows(products)}</tbody>;
+TBody.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.exact({
+      name: PropTypes.string,
+      price: PropTypes.string,
+      stocked: PropTypes.bool,
+      category: PropTypes.string,
+    })
+  ),
 };
 
 export default TBody;
