4f2d357e04cee16d81185c4cf3227e58530de068
colby <colby88cardell@gmail.com>
2021-02-25 17:48:43 -0500

    Handle Key up for search bar

--- src/components/FilterableProductTable/index.js
+++ src/components/FilterableProductTable/index.js
@@ -1,11 +1,20 @@
 import Table from "./ProductTable";
 import Search from "./SearchBar";
 
-const FilterableProductTable = () => (
-  <main className="container mx-auto w-1/2 flex flex-col items-center mt-8">
-    <Search />
-    <Table />
-  </main>
-);
+const FilterableProductTable = () => {
+  function handleKeyUp({ target: { value } }) {
+    console.log(value);
+  }
+  function handleChange({ target: { checked } }) {
+    console.log(checked);
+  }
+
+  return (
+    <main className="container mx-auto w-1/2 flex flex-col items-center mt-8">
+      <Search searchHandler={handleKeyUp} inStockHandler={handleChange} />
+      <Table />
+    </main>
+  );
+};
 
 export default FilterableProductTable;
